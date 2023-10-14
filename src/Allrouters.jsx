cba9a05c92866095ad399a4193baae45075fd31a
justsmruti52 <justsmruti52@gmail.com>
2023-10-14 20:47:20 +0530

    second

--- src/Allrouters.jsx
+++ src/Allrouters.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
-import Maintain from './pages/Maintain'
-import Reports from './pages/Reports'
-import Trasaction from './pages/Trasaction'
 import Logout from './pages/Logout'
 import Signup from './pages/Signup'
 import Loginhome from './pages/Loginhome'
@@ -19,13 +16,7 @@ const Allrouters = () => {
     <Route path='/login' element={<Login/>}></Route>
     <Route path='/signup' element={<Signup/>}></Route>
     <Route path='/loginhome' element={<Loginhome/>}></Route>
-
     <Route path = '/setreminder' element={<SetReminderPage/>}> </Route>
-
-
-    <Route path='/maintenance' element={<Maintain/>}></Route>
-    <Route path='/reports' element={<Reports/>}></Route>
-    <Route path='/trasaction' element={<Trasaction/>}></Route>
     <Route path='/logout' element={<Logout/>}></Route>
     <Route path='/modify' element={<Modify/>}></Route>
 </Routes>
